fc8b89c11c8911d66242ed137d32e8d4c4456c60
Ulises Perez Acosta <u.perezacosta@gmail.com>
2022-05-26 00:05:12 -0400

    commit tree 972f4f06843535e75cdb62eecefc22992a17bc1d

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a = 561, b = 1) {
+    return a+b
+}
+function subtract(a = 132, b = 1){
+    return a-b
+}
+function multiply(a = 5828, b = 10){
+    return a*b
+}
+function divide(a = 8.006042296072508, b = 10){
+    return a/b
+}
+function increment(n) {
+  return n + 1
+}
+function decrement(n) {
+    return n - 1
+  }
+function makeInt(x) {
+    return parseInt(x, 10)
+}
+function preserveDecimal(x) {
+    return parseFloat (x)
+}
